499a1d02de064604a37a7a00d7bde5294cf9d4a3
Ernesto Cirilo <ernestoc@n-compass.biz>
2024-06-14 15:02:13 +0800

    jpeg media only

--- src/app/services/api/endpoints/tier1/images.ts
+++ src/app/services/api/endpoints/tier1/images.ts
@@ -1,4 +1,3 @@
-
 import { Images } from "../../models/Images";
 import { tier1Api } from "../../tier1";
 
@@ -6,11 +5,12 @@ const extendedApi = tier1Api.injectEndpoints({
   endpoints: (builder) => ({
     getContents: builder.query<Array<Images>, void>({
       query: () => `/content`,
+      transformResponse: (response: Array<Images>) => {
+        return response.filter((f) => f.fileType != "image/gif");
+      },
     }),
   }),
   overrideExisting: false,
 });
 
-export const {
-    useLazyGetContentsQuery
-} = extendedApi;
+export const { useLazyGetContentsQuery } = extendedApi;
